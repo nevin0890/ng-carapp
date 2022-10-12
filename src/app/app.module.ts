8cb41057aa4c8946bea73d58c2f3a4a352d7c957
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-12 18:59:45 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarsComponent } from './cars/cars.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'cars', component: CarsComponent },
+ 
+  { path: '', component: CarsComponent }
 
 ]
 @NgModule({
@@ -22,7 +22,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
 
   ],
   providers: [],
