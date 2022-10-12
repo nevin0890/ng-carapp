206c482d78be17358ab87ee3bff46851e882ae36
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-12 18:33:04 +0530

    2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
-  
+
 ]
 @NgModule({
   declarations: [
